4118d5de75db2812f20ce453bab6f0126b193cd7
Rajia Rani <rajiarani.syal@gmail.com>
2024-02-11 00:37:14 -0600

    added

---
+++ CS50WebProgrammingwithPythonandJavaScript./Python/4.sequences.py
@@ -0,0 +1,13 @@
+# there are a number of different types of sequences
+# that all obey similar properties.
+name = "rajia rani"
+print(name[0])
+#I can store that information inside of a list in Python.
+names = ["raj", "reet", "geet", "heena"]
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+
+# Tuple is often used if you have a couple of values that aren't going to change but you need to store
+# a pair of values like two values together,or three values together, or something like it.
